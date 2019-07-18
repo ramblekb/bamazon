76f0e857f9db4e1d457066fe13d3e0ecbfef2dd1
Kip Bradley Smith <ramblekb@gmail.com>
2019-07-17 23:00:13 -0500

    not connected

--- bam.js
+++ bam.js
@@ -1,8 +1,8 @@
-var mysql = require("mysql");
+var MySQL = require("MySQL");
 var inquirer = require("inquirer");
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+var connection = MySQL.createConnection({
   host: 'localhost',
 
   // Your port; if not 3306
@@ -14,11 +14,71 @@ var connection = mysql.createConnection({
   // Your password
   password: "Kbs120285",
   database: "bamazon"
-});
-
+})
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-  if (err) throw err;
-//  run the start function after the connection is made to prompt the user
-  start();
-});
+  if(err) throw err;
+  console.log("connection successful");
+  makeTable();
+})
+var makeTable =function(){
+  connection.query("SELECT * FROM products", function(err,res){
+    for(var i=0; i<res.length; i++){
+      console.log(res[i].itemid+" || "+res[i].productname+" || "+
+      res[i].departmentname+" || "+res[i].price+" || "+res[i],
+      stockquantity+"\n");
+    }
+    promptCustomer(res);
+    })
+  }
+
+var promptCustomer = function(res){
+  inquirer.prompt([{
+    type:'input',
+    name:'choice',
+    message:"What would you like to buy? [Quit with Q]"
+  }]).then(function(answer){
+    var correct = false;
+    if(answer.choice.toUpperCase()=="Q"){
+      process.exit()
+    }
+    for(var i=0;i<res.length;i++){
+      if(res[i].productname==answer.choice){
+        correct=true;
+        var product=answer.choice;
+        var id=i;
+        inquirer.prompt({
+          type:"input",
+          name:"quant",
+          message:"How many would you like?",
+          validate: function(value){
+            if(isNaN(value)==false){
+              return true;
+            } else {
+              return false;
+            }
+            }
+          }).then(function(answer){
+            if((res[id].stockquantity-answer.quant)>0){
+              connection.query("UPDATE products SET stockquantity='"+(res[id].stockquantity-
+                answer.quant)+"' WHERE productname='"+product
+                +"'", function(err,res2){
+                console.log("Purchase successful!");
+                makeTable();
+              })
+            } else {
+              console.log("something went wrong!");
+              promptCustomer(res);
+
+            }
+              })
+            }
+          }
+          if(i==res.length && correct==false){
+            console.log("Not a valid choice");
+            promptCustomer(res);
+          }
+        })
+      }
+            
+            
