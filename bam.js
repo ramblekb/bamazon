1f802a466acb6cb433aab684678cd0ed8d3420bb
Kip Bradley Smith <ramblekb@gmail.com>
2019-07-19 20:53:36 -0500

    work

--- bam.js
+++ bam.js
@@ -1,9 +1,9 @@
-var MySQL = require("MySQL");
+var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 // create the connection information for the sql database
-var connection = MySQL.createConnection({
-  host: 'localhost',
+var connection = mysql.createConnection({
+  host: "localhost",
 
   // Your port; if not 3306
   port: 3306,
@@ -14,71 +14,212 @@ var connection = MySQL.createConnection({
   // Your password
   password: "Kbs120285",
   database: "bamazon"
-})
+});
+
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-  if(err) throw err;
-  console.log("connection successful");
-  makeTable();
-})
-var makeTable =function(){
-  connection.query("SELECT * FROM products", function(err,res){
-    for(var i=0; i<res.length; i++){
-      console.log(res[i].itemid+" || "+res[i].productname+" || "+
-      res[i].departmentname+" || "+res[i].price+" || "+res[i],
-      stockquantity+"\n");
-    }
-    promptCustomer(res);
+  if (err) throw err;
+  // console.log("connected");
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to buy?",
+      choices: [
+        "Red Apples",
+        "Green Apples",
+        "Strawberries",
+        "Blueberries",
+        "Blackberries",
+        "Pears",
+        "Peaches",
+        "Pinnapples",
+        "Bannanas"
+      ]
     })
-  }
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Red Apples":
+        redapplesSearch();
+        break;
 
-var promptCustomer = function(res){
-  inquirer.prompt([{
-    type:'input',
-    name:'choice',
-    message:"What would you like to buy? [Quit with Q]"
-  }]).then(function(answer){
-    var correct = false;
-    if(answer.choice.toUpperCase()=="Q"){
-      process.exit()
-    }
-    for(var i=0;i<res.length;i++){
-      if(res[i].productname==answer.choice){
-        correct=true;
-        var product=answer.choice;
-        var id=i;
-        inquirer.prompt({
-          type:"input",
-          name:"quant",
-          message:"How many would you like?",
-          validate: function(value){
-            if(isNaN(value)==false){
-              return true;
-            } else {
-              return false;
-            }
-            }
-          }).then(function(answer){
-            if((res[id].stockquantity-answer.quant)>0){
-              connection.query("UPDATE products SET stockquantity='"+(res[id].stockquantity-
-                answer.quant)+"' WHERE productname= '"+product
-                +"'", function(err,res2){
-                console.log("Purchase successful!");
-                makeTable();
-              })
-            } else {
-              console.log("something went wrong!");
-              promptCustomer(res);
+      case "Find all artists who appear more than once":
+        multiSearch();
+        break;
 
-            }
-              })
-            }
-          }
-          if(i==res.length && correct==false){
-            console.log("Not a valid choice");
-            promptCustomer(res);
-          }
-        })
+      case "Find data within a specific range":
+        rangeSearch();
+        break;
+
+      case "Search for a specific song":
+        songSearch();
+        break;
+
+      case "Find artists with a top song and top album in the same year":
+        songAndAlbumSearch();
+        break;
       }
-            
-            
+    });
+}
+
+function redapplesSearch() {
+  inquirer
+    .prompt({
+      name: "red apples",
+      type: "input",
+      message: "How many red apples would you like to buy?"
+    })
+    .then(function(answer) {
+      var query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { redapples: answer.redapples }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+        }
+        runSearch();
+      });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+// function start() {
+//   inquirer
+//     .prompt({
+//       name: "postOrBid",
+//       type: "list",
+//       message: "Would you like to [POST] an auction or [BID] on an auction?",
+//       choices: ["POST", "BID", "EXIT"]
+//     })
+//     .then(function(answer) {
+//       // based on their answer, either call the bid or the post functions
+//       if (answer.postOrBid === "POST") {
+//         postAuction();
+//       }
+//       else if(answer.postOrBid === "BID") {
+//         bidAuction();
+//       } else{
+//         connection.end();
+//       }
+//     });
+// }
+
+// // function to handle posting new items up for auction
+// function postAuction() {
+//   // prompt for info about the item being put up for auction
+//   inquirer
+//     .prompt([
+//       {
+//         name: "item",
+//         type: "input",
+//         message: "What is the item you would like to submit?"
+//       },
+//       {
+//         name: "category",
+//         type: "input",
+//         message: "What category would you like to place your auction in?"
+//       },
+//       {
+//         name: "startingBid",
+//         type: "input",
+//         message: "What would you like your starting bid to be?",
+//         validate: function(value) {
+//           if (isNaN(value) === false) {
+//             return true;
+//           }
+//           return false;
+//         }
+//       }
+//     ])
+//     .then(function(answer) {
+//       // when finished prompting, insert a new item into the db with that info
+//       connection.query(
+//         "INSERT INTO auctions SET ?",
+//         {
+//           item_name: answer.item,
+//           category: answer.category,
+//           starting_bid: answer.startingBid || 0,
+//           highest_bid: answer.startingBid || 0
+//         },
+//         function(err) {
+//           if (err) throw err;
+//           console.log("Your auction was created successfully!");
+//           // re-prompt the user for if they want to bid or post
+//           start();
+//         }
+//       );
+//     });
+// }
+
+// function bidAuction() {
+//   // query the database for all items being auctioned
+//   connection.query("SELECT * FROM auctions", function(err, results) {
+//     if (err) throw err;
+//     // once you have the items, prompt the user for which they'd like to bid on
+//     inquirer
+//       .prompt([
+//         {
+//           name: "choice",
+//           type: "rawlist",
+//           choices: function() {
+//             var choiceArray = [];
+//             for (var i = 0; i < results.length; i++) {
+//               choiceArray.push(results[i].item_name);
+//             }
+//             return choiceArray;
+//           },
+//           message: "What auction would you like to place a bid in?"
+//         },
+//         {
+//           name: "bid",
+//           type: "input",
+//           message: "How much would you like to bid?"
+//         }
+//       ])
+//       .then(function(answer) {
+//         // get the information of the chosen item
+//         var chosenItem;
+//         for (var i = 0; i < results.length; i++) {
+//           if (results[i].item_name === answer.choice) {
+//             chosenItem = results[i];
+//           }
+//         }
+
+//         // determine if bid was high enough
+//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
+//           // bid was high enough, so update db, let the user know, and start over
+//           connection.query(
+//             "UPDATE auctions SET ? WHERE ?",
+//             [
+//               {
+//                 highest_bid: answer.bid
+//               },
+//               {
+//                 id: chosenItem.id
+//               }
+//             ],
+//             function(error) {
+//               if (error) throw err;
+//               console.log("Bid placed successfully!");
+//               start();
+//             }
+//           );
+//         }
+//         else {
+//           // bid wasn't high enough, so apologize and start over
+//           console.log("Your bid was too low. Try again...");
+//           start();
+//         }
+//       });
+//   });
+// }
